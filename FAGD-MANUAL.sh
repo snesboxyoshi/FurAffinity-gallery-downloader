92cc5d4a8ecc96ca827d9d6fccd3c8a1ad5470fe
Snessy el programador <ricardolosano97@gmail.com>
2022-04-10 23:44:26 -0500

    FAGD but without the idiot proof bloat
    Finally! after popular demand by the eediot that helped me with the HTML crap, I decided to make a version for people who actually know how to Linux and  don't need someone to hold their hand on something as simple as getting the right packages and starting up a service.

---
+++ FAGD-MANUAL.sh
@@ -0,0 +1,12 @@
+clear
+echo -e "Welcome to the manual version of the FurAffinity gallery downloader! This version assumes that you have more than 2 brain cells, know how to install the required packages for this to work (torsocks, grep, parallel, tor, wget.), and know how to enable and start the ((tor)) service under whatever thing your distro uses to run services (Since most mainstream distros use systemd, it would be: systemctl enable tor | systemctl start tor."
+echo ""
+echo "Please insert the name presented in the URL of the artist you wanna save (example:dragoneer)"
+read name
+mkdir $name
+sudo chmod 777 $name
+cd "$name"
+torsocks -P 9050 curl http://g6jy5jkx466lrqojcngbnksugrcfxsl562bzuikrka5rv7srgguqbjid.onion/fa/$name/ | grep -Po '(?<=href=")[^"]*(?=")'| parallel torsocks wget --no-clobber http://g6jy5jkx466lrqojcngbnksugrcfxsl562bzuikrka5rv7srgguqbjid.onion/fa/$name/{}
+clear
+echo "$name's art has been saved in the /$name/ directory where you placed the script. Enjoy the art!"
+exit
